867f70520bb7b3e2d9036e7bd218dd0f83f3955c
장영욱 <raeben@naver.com>
2023-12-28 23:48:43 +0900

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -169,12 +169,6 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: '/',
     proxy: true, // proxy 사용
-    headers: {
-      common: {
-        Authorization:
-          'Bearer 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',
-      },
-    },
   },
   proxy: {
     '/stabilityApi': {
